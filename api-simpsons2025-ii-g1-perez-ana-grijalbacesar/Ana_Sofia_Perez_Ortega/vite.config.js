287b7f40cd6615761eaac67476f31663023ce178
CesarGrijalba <c.grijalba@udla.edu.co>
2025-10-16 16:26:45 -0500

    Set base path for Vite configuration

--- api-simpsons2025-ii-g1-perez-ana-grijalbacesar/Ana_Sofia_Perez_Ortega/vite.config.js
+++ api-simpsons2025-ii-g1-perez-ana-grijalbacesar/Ana_Sofia_Perez_Ortega/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ApiSimpsons_2025IIg1_Perez_Sofia_Grijalba_Cesar/',
   plugins: [react()],
 })
